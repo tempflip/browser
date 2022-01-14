8098d3569f43ea0c1e74080936abde0da0697322
Peter Tempfli <peter.tempfli@deliveryhero.com>
2022-01-14 16:37:40 +0100

    w

--- src/Page.ts
+++ src/Page.ts
@@ -1,10 +1,61 @@
+import { throws } from "assert";
+import { hostname } from "os";
+
+const https = require('https');
+
+interface urlParam {
+    hostname: string,
+    path: string
+}
+
 export class Page {
-    url;
-    constructor(url : string) {
-        this.url = url;
+    data: Buffer;
+    headers;
+    location : urlParam = {hostname : '', path : ''};
+
+    constructor(url: string) {
+        this.setLocation(url);
+    }
+
+    private setLocation(url: string) {
+        let locElems = url.split('/');
+        locElems.shift();
+        locElems.shift();
+        this.location.hostname = locElems.shift();
+        this.location.path = '/' + locElems.join('/');
+        console.log(this.location);
     }
 
     download() {
-        console.log('## downloading...', this.url);
+        let pr = new Promise((resolve, reject) => {
+            let req = https.request({
+                hostname: this.location.hostname,
+                port: 443,
+                path: this.location.path,
+                method: 'GET'
+            }, res => {
+                console.log(res.statusCode);
+                this.headers = res.headers;
+
+                if (res.statusCode == '301') {
+                    console.log('moved,', this.headers.location);
+                    this.setLocation(this.headers.location);
+                    this.download().then(resolve);
+                } else {
+
+                    res.on('data', d => {
+                        this.data = d;
+                        resolve(true);
+                    });
+                }
+            });
+
+            req.end();
+        })
+        return pr;
+    }
+
+    get asText(): string {
+        return this.data.toString();
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { Page } from './Page'
 
-let p : Page = new Page('https://wikipedia.org/');
-p.download();
+let p: Page = new Page('https://wikipedia.org/');
+
+p.download().then(() => {
+    console.log('Success!');
+    // console.log(p.headers);
+    console.log(p.asText);
+});
